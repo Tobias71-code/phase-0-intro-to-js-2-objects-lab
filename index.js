ff82f96a7aeec3f9556cc2f057f181188f454dbb
TobiasNethan <sbttobias@gmail.com>
2024-10-03 16:02:25 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+// Employee object
+const employee = {
+    name: "Sam",
+  };
+  
+  // Non-destructive function to update employee
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    // Return a new object by copying employee and adding the new key-value pair
+    return { ...employee, [key]: value };
+  }
+  
+  // Destructive function to update employee
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    // Directly modify the employee object
+    employee[key] = value;
+    return employee;
+  }
+  
+  // Non-destructive function to delete a key from employee
+  function deleteFromEmployeeByKey(employee, key) {
+    // Create a copy of the employee object and delete the key from the new object
+    let newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // Destructive function to delete a key from employee
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    // Delete the key from the original employee object
+    delete employee[key];
+    return employee;
+  }
+  
